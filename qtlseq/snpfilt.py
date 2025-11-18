1c49c527cc0df0845b19a7dd1faf552717593520
Del Hannay <158506572+bu20dy@users.noreply.github.com>
2025-11-18 14:41:02 -0700

    Update snpfilt.py
    Added in more filtering options

--- qtlseq/snpfilt.py
+++ qtlseq/snpfilt.py
@@ -5,19 +5,22 @@ class SnpFilt(object):
         self.maxDP = args.max_depth
         self.minDP = args.min_depth
         self.strand_bias = args.strand_bias
+        self.parental_SNPfilter = args.parental_SNPfilter
 
     def filt_parent_GT(self, parent_GT, bulk1_AD, bulk2_AD):
+        mode = self.parental_SNPfilter
         record = {}
-        # only use homo in cultivar
-        if parent_GT in ['0/0', '0|0', '1/1', '1|1']:
-            bulk1_ADs = bulk1_AD.split(',')
-            bulk2_ADs = bulk2_AD.split(',')
-            # check biallele or multi-allele
-            if len(bulk1_ADs) == 2:
-                # filter missing
-                if (not '.' in bulk1_ADs) and (not '.' in bulk2_ADs):
-                    # check whether REF homo or ALT homo.
-                    if parent_GT in ['0/0', '0|0']:
+        if mode == "hetero":
+        # hetero == keeps SNPs where the parent is heterozygous
+        # (e.g. self-cross). removes homozygous SNPs.
+        # does not re-assign bulk genotypes.
+            if parent_GT in ['0/1', '0|1']:
+                bulk1_ADs = bulk1_AD.split(',')
+                bulk2_ADs = bulk2_AD.split(',')
+                # ensure biallelic (does not handle multialletic SNPs)
+                if len(bulk1_ADs) == 2:
+                    # filter missing
+                    if (not '.' in bulk1_ADs) and (not '.' in bulk2_ADs):
                         record['bulk1_ref_AD'] = int(bulk1_ADs[0])
                         record['bulk1_alt_AD'] = int(bulk1_ADs[1])
                         record['bulk2_ref_AD'] = int(bulk2_ADs[0])
@@ -26,36 +29,30 @@ class SnpFilt(object):
                         # it will be discarded because SNP-index will be zero.
                         if (record['bulk1_ref_AD'] != 0 or record['bulk2_ref_AD'] != 0) and \
                            (record['bulk1_alt_AD'] != 0 or record['bulk2_alt_AD'] != 0):
-                            record['parent_GT'] = '0/0'
+                            record['parent_GT'] = '0/1'
                             record['type'] = 'keep'
                         else:
                             record['type'] = 'discard'
                     else:
-                        record['bulk1_ref_AD'] = int(bulk1_ADs[1])
-                        record['bulk1_alt_AD'] = int(bulk1_ADs[0])
-                        record['bulk2_ref_AD'] = int(bulk2_ADs[1])
-                        record['bulk2_alt_AD'] = int(bulk2_ADs[0])
-                        # if depth of REF is zero in bulk,
+                        record['type'] = 'discard'
+                else:
+                    record['type'] = 'discard'
+            elif parent_GT in ['1/0', '1|0']:
+                bulk1_ADs = bulk1_AD.split(',')
+                bulk2_ADs = bulk2_AD.split(',')
+                # ensure biallelic (does not handle multialletic SNPs)
+                if len(bulk1_ADs) == 2:
+                    # filter missing
+                    if (not '.' in bulk1_ADs) and (not '.' in bulk2_ADs):
+                        record['bulk1_ref_AD'] = int(bulk1_ADs[0])
+                        record['bulk1_alt_AD'] = int(bulk1_ADs[1])
+                        record['bulk2_ref_AD'] = int(bulk2_ADs[0])
+                        record['bulk2_alt_AD'] = int(bulk2_ADs[1])
+                        # if depths of REF or ALT are zero in both bulks,
                         # it will be discarded because SNP-index will be zero.
                         if (record['bulk1_ref_AD'] != 0 or record['bulk2_ref_AD'] != 0) and \
                            (record['bulk1_alt_AD'] != 0 or record['bulk2_alt_AD'] != 0):
-                            record['parent_GT'] = '1/1'
-                            record['type'] = 'keep'
-                        else:
-                            record['type'] = 'discard'
-                else:
-                    record['type'] = 'discard'
-            # check ALT homo in cultivar
-            elif len(bulk1_ADs) == 3:
-                # filter missing
-                if (not '.' in bulk1_ADs) and (not '.' in bulk2_ADs):
-                    if parent_GT in ['1/1', '1|1']:
-                        if (bulk1_ADs[0] == '0') and (bulk2_ADs[0] == '0'):
-                            record['bulk1_ref_AD'] = int(bulk1_ADs[1])
-                            record['bulk1_alt_AD'] = int(bulk1_ADs[2])
-                            record['bulk2_ref_AD'] = int(bulk2_ADs[1])
-                            record['bulk2_alt_AD'] = int(bulk2_ADs[2])
-                            record['parent_GT'] = '1/1'
+                            record['parent_GT'] = '1/0'
                             record['type'] = 'keep'
                         else:
                             record['type'] = 'discard'
@@ -65,8 +62,54 @@ class SnpFilt(object):
                     record['type'] = 'discard'
             else:
                 record['type'] = 'discard'
+                    
         else:
-            record['type'] = 'discard'
+        # homo == keep SNPs where the parent has 0/0 or 1/1 genotype
+        # and removes sites where parent is heterozygous.
+        # this will also re-record bulk genotype assignments to be in reference
+        # to the parental genotype. For example, if bulk 1 has the same genotype as
+        # the parent, the bulk genotype for that SNP will be 0/0.
+        # THIS IS THE DEFAULT AND HOW QTL-SEQ BY SUGIHARA 2022 OPERATES.
+            if parent_GT in ['0/0', '0|0', '1/1', '1|1']:
+                bulk1_ADs = bulk1_AD.split(',')
+                bulk2_ADs = bulk2_AD.split(',')
+                # check biallele or multi-allele
+                if len(bulk1_ADs) == 2:
+                    # filter missing
+                    if (not '.' in bulk1_ADs) and (not '.' in bulk2_ADs):
+                        # check whether REF homo or ALT homo.
+                        if parent_GT in ['0/0', '0|0']:
+                            record['bulk1_ref_AD'] = int(bulk1_ADs[0])
+                            record['bulk1_alt_AD'] = int(bulk1_ADs[1])
+                            record['bulk2_ref_AD'] = int(bulk2_ADs[0])
+                            record['bulk2_alt_AD'] = int(bulk2_ADs[1])
+                            # if depths of REF or ALT are zero in both bulks,
+                            # it will be discarded because SNP-index will be zero.
+                            if (record['bulk1_ref_AD'] != 0 or record['bulk2_ref_AD'] != 0) and \
+                               (record['bulk1_alt_AD'] != 0 or record['bulk2_alt_AD'] != 0):
+                                record['parent_GT'] = '0/0'
+                                record['type'] = 'keep'
+                            else:
+                                record['type'] = 'discard'
+                        else:
+                            record['bulk1_ref_AD'] = int(bulk1_ADs[1])
+                            record['bulk1_alt_AD'] = int(bulk1_ADs[0])
+                            record['bulk2_ref_AD'] = int(bulk2_ADs[1])
+                            record['bulk2_alt_AD'] = int(bulk2_ADs[0])
+                            # if depth of REF is zero in bulk,
+                            # it will be discarded because SNP-index will be zero.
+                            if (record['bulk1_ref_AD'] != 0 or record['bulk2_ref_AD'] != 0) and \
+                               (record['bulk1_alt_AD'] != 0 or record['bulk2_alt_AD'] != 0):
+                                record['parent_GT'] = '1/1'
+                                record['type'] = 'keep'
+                            else:
+                                record['type'] = 'discard'
+                    else:
+                        record['type'] = 'discard'
+                else:
+                    record['type'] = 'discard'
+            else:
+                record['type'] = 'discard'
         return record
 
     def filt_depth(self, record, parent_AD):
@@ -97,6 +140,12 @@ class SnpFilt(object):
 
         if record['parent_GT'] == '0/0':
             cultivar_ADF = int(ADF.split(',')[0])
+            cultivar_ADR = int(ADR.split(',')[0])
+        elif record['parent_GT'] == '0/1':
+            cultivar_ADF = int(ADF.split(',')[0])
+            cultivar_ADR = int(ADR.split(',')[1])
+        elif record['parent_GT'] == '1/0':
+            cultivar_ADF = int(ADF.split(',')[1])
             cultivar_ADR = int(ADR.split(',')[0])
         else:
             cultivar_ADF = int(ADF.split(',')[1])
